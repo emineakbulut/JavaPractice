0ace82da7d863aa3027ea90327ee51178f81f924
emineakbulut <ezz.akbulut@gmail.com>
2021-06-14 16:32:37 -0700

    After Qualitest

---
+++ src/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+
+public void model (){
+
+    }
+public  void  color(){
+
+}
+
+public void make (){
+
+
+}
+
+class myCar extends Car{
+
+
+    myCar(int model,String color, String make){
+
+
+    }
+
+
+}
+class Test{
+
+    Car car= new myCar(2009,"grey","toyota");
+}
+}
